c21bd39a9317db52a52ea72772defa7bfd45c0a7
Ved Vitthal Shukla <ved.vitthal@inito.com>
2023-07-20 01:16:22 +0530

    Added Monthly and Daily Update Function, added age and account opening amount check

--- Accounts.h
+++ Accounts.h
@@ -14,6 +14,7 @@ public:
     m_CVV_Number = s_CVV_Number_Generator.Get();
 
     m_Number_Of_Withdraw_This_Month = 0;
+    m_Activity_This_Month = 0;
 
     m_Remaining_Day_Withdraw_Limit = sc_Withdraw_Amount_Per_Day_Upper_Bonund;
 
@@ -21,13 +22,13 @@ public:
   }
 
 public:
-  long long Get_ID() { return m_Account_Number; }
-
-public:
   long long Get_Account_Number() { return m_Account_Number; }
   long long Get_ATM_Number() { return m_ATM_Card_Number; }
   long long Get_Current_Balance() { return m_Current_Balance; }
   long long Get_Customer_ID() { return m_Customer_ID; }
+
+public:
+  long long Get_ID() { return m_Account_Number; }
 
 public:
   bool Withdraw(int Withdraw_Amount) {
@@ -95,6 +96,7 @@ public:
     m_Current_Balance -= Withdraw_Amount;
     ++m_Number_Of_Withdraw_This_Month;
     m_Remaining_Day_Withdraw_Limit -= Withdraw_Amount;
+    m_Activity_This_Month += Withdraw_Amount;
 
     std::cout << "Successfully withdrawn: " << Withdraw_Amount
               << ". New Balance is: " << m_Current_Balance << std::endl;
@@ -106,6 +108,7 @@ public:
 
   bool Deposite(int Deposite_Amount) {
     m_Current_Balance += Deposite_Amount;
+    m_Activity_This_Month += Deposite_Amount;
 
     std::cout << GREEN << "Amount Deposited!" << RESET << std::endl;
     std::cout << Deposite_Amount << " deposited. "
@@ -121,6 +124,7 @@ public:
               << std::endl;
 
     m_Current_Balance += Transaction_Amount;
+    m_Activity_This_Month += Transaction_Amount;
 
     std::cout << "Amount: " << Transaction_Amount << ". received..."
               << std::endl;
@@ -147,7 +151,42 @@ public:
 
     p_Other->Get_Transaction_Amount(Transaction_Amount, this);
 
+    m_Activity_This_Month += Transaction_Amount;
+
     return true;
+  }
+
+public:
+  void Month_End() {
+    {
+      // NRV
+      int NRV_Shortfall = std::max(0, sc_Net_Relationship_Value_Per_Month -
+                                          m_Activity_This_Month) /
+                          1000;
+      int NRV_Penalty = NRV_Shortfall * 1000;
+
+      if (NRV_Penalty) {
+        std::cout << BOLD_RED << "***** Lack of Activity *****" << RESET
+                  << std::endl;
+        std::cout << "NRV Penalty: " << NRV_Penalty << " charged." << std::endl;
+
+        m_Current_Balance -= NRV_Shortfall * 1000;
+      }
+    }
+
+    {
+      // Add interest
+      double Interest_Rate_Per_Month = sc_Interest_Rate / 12.0;
+      int Monthly_Interest = m_Current_Balance * Interest_Rate_Per_Month;
+      m_Current_Balance += Monthly_Interest;
+    }
+
+    m_Number_Of_Withdraw_This_Month = 0;
+    m_Activity_This_Month = 0;
+  }
+
+  void Day_End() {
+    m_Remaining_Day_Withdraw_Limit = sc_Withdraw_Amount_Per_Day_Upper_Bonund;
   }
 
 public:
@@ -165,7 +204,18 @@ public:
               << std::endl;
   }
 
+public:
+  static bool Check_Age(int Age) {
+    return (Age >= sc_Minimum_Age_To_Open_Account);
+  }
+  static bool Check_Opening_Amount(int Opening_Amount) {
+    return (Opening_Amount >= sc_Minimum_Amount_To_Open_Account);
+  }
+
 private:
+  static const int sc_Minimum_Age_To_Open_Account = 0;
+  static const int sc_Minimum_Amount_To_Open_Account = 10000;
+
   static const int sc_Interest_Rate = 6;
 
   static const int sc_Minimum_Opening_Amount = 10000;
@@ -193,6 +243,7 @@ private:
   int m_Current_Balance;
 
   int m_Number_Of_Withdraw_This_Month;
+  int m_Activity_This_Month;
   int m_Remaining_Day_Withdraw_Limit;
 
   std::vector<std::pair<std::string, int>> m_Transaction_History;
